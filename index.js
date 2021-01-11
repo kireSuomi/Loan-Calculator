7b8c00912a7da1e9e63f42f80dcd2aff5579b42c
Erik <kireSuomi@gmail.com>
2021-01-11 09:49:45 +0100

    Code splitting  + extra interest

--- index.js
+++ index.js
@@ -29,27 +29,72 @@ class LoanCalculator {
         if (overMultiple) amortization += ( 0.01 * loan_amount ) /12
         return amortization
     }
+    
+    GetTaxReduction(interest){
+        if (this.loanAmount > 100_000) {
+            return interest * 0.79;
+        } else {
+            return interest * 0.7
+        }
+    }
+
+    GetPaymentAmount(interest, amortization) {
+        let amount = (interest + amortization) % 0.5 > 0.5 ? Math.ceil(interest + amortization) : interest + amortization
+        if (amount % 1 != 0) {
+            amount = payment_amount.toFixed(0)
+        }
+        return amount
+    }
+
+    GetInterest(extraInterest = 0){
+        let interest;
+
+        if (extraInterest !== 0) {
+            interest = this.loanLeft * (this.monthlyInterestRate + extraInterest)
+        } else {
+            interest = this.loanLeft * this.monthlyInterestRate
+        }
+        return interest
+    }
+
 
     PaymentAmount(overIncomeMultiple) {
         const loan_to_value = this.loanLeft / this.houseValue
         const amortization = this.Amortization(loan_to_value, overIncomeMultiple)
-        let interest = (this.loanLeft * this.monthlyInterestRate)
+        const interest = this.GetInterest()
 
-        let payment_amount = (interest + amortization) % 0.5 > 0.5 ? Math.ceil(interest + amortization) : interest + amortization
-        if (payment_amount % 1 != 0) {
-            payment_amount = payment_amount.toFixed(0)
-        }
-        this.loanLeft -= amortization
+        const interest_plus_1 = this.GetInterest(0.01)
+        const interest_plus_2 = this.GetInterest(0.02)
+        const interest_plus_3 = this.GetInterest(0.03)
+
+        const interest_post_tax = this.GetTaxReduction(interest);
+    
+        const payment_amount = this.GetPaymentAmount(interest, amortization)
+        const paymeny_amount_post_tax = this.GetPaymentAmount(interest_post_tax, amortization)
+
+        //Payment amount but with extra interest rates
+        const payment_amount_1 = this.GetPaymentAmount(interest_plus_1, amortization)
+        const payment_amount_2 = this.GetPaymentAmount(interest_plus_2, amortization)
+        const payment_amount_3 = this.GetPaymentAmount(interest_plus_3, amortization)
+
+        //this.loanLeft -= amortization
         return {
             interest: interest,
+            interest_post_tax: interest_post_tax,
             amortization: amortization,
-            loanLeft :this.loanLeft,
+            //loanLeft :this.loanLeft,
             ltv : loan_to_value,
             paymentAmount: payment_amount,
+            paymentAmountTax: paymeny_amount_post_tax,
             staticData: {
                 loanAmount: this.loanAmount,
                 houseValue: this.houseValue,
                 interest: this.interestRate
+            },
+            extraInterest:  {
+                extraInterest1: payment_amount_1,
+                extraInterest2: payment_amount_2,
+                extraInterest3: payment_amount_3
             }
         }
     }
@@ -71,4 +116,6 @@ class LoanCalculator {
 
 const Calculator = new LoanCalculator(6_000_000, 10_000_000, 1.5)
 const month = Calculator.PaymentAmount()
+const month2 = Calculator.PaymentAmount(true)
 console.log(month);
+console.log(month2);
